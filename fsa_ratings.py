68ec0e64082871207393ab566bec39cf5fa06780
pdizzle10 <patricklynott01@gmail.com>
2023-06-18 17:12:50 +0100

    updated figures to include all leeds records

--- fsa_ratings.py
+++ fsa_ratings.py
@@ -5,21 +5,25 @@ import matplotlib.pyplot as plt
 import requests
 import os
 
-url = "https://ratings.food.gov.uk/search/%5E/leeds/1/100/json"
+#get data from API and create DataFrame
+url = "https://ratings.food.gov.uk/search/%5E/leeds/1/10000/json"
 r = requests.get(url)
 df = pd.DataFrame.from_dict(r.json()["FHRSEstablishment"]["EstablishmentCollection"]['EstablishmentDetail'])
 
-df = df[(df["RatingValue"] != "AwaitingInspection") & (df["RatingValue"] != "Exempt")]
+#clean DataFrame by removing businesses that have not been given a rating and converting the Scores column from a Dictionary into 3 separate columns
+df = df[(df["RatingValue"] != "AwaitingInspection") & (df["RatingValue"] != "Exempt") & (df["RatingValue"] != "Pass") & (df["LocalAuthorityName"] == "Leeds")]
 df = pd.concat([df.drop("Scores", axis=1), df["Scores"].apply(pd.Series)], axis=1)
 
+#turn the ratings columns to integers
 df["Hygiene"] = pd.to_numeric(df["Hygiene"])
 df["Structural"] = pd.to_numeric(df["Structural"])
 df["ConfidenceInManagement"] = pd.to_numeric(df["ConfidenceInManagement"])
 
+#create a second dataframe to be used specifically for the bar chart which aggregates and displays averages for each of the ratings. convert this to an easy to use format for seaborn
 bars = round(df.groupby("BusinessType", as_index=False)[["BusinessType","Hygiene","Structural","ConfidenceInManagement"]].mean(),1)
 bars = bars.melt("BusinessType", var_name="ratings", value_name="values")
 
-
+#first chart plotting average food safety ratings by business type
 sns.set(context="notebook")
 sns.set_style("dark")
 sns.barplot(data=bars, y="BusinessType", x="values", hue="ratings", color="c").set(ylabel = "Business Type",
@@ -29,6 +33,8 @@ plt.legend(title="Food Safety Ratings")
 plt.savefig("Average Food Safety Ratings.png")
 plt.clf()
 
+
+#second group of charts counting scores across the ratings categories 
 ratings = ["Hygiene","Structural","ConfidenceInManagement"]
 
 for rating in ratings:
